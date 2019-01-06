62ba9d97cfeeb15468c3e0a30819ce1925c9c40a
shubham kakkar <shubham.kakkar98@gmail.com>
2019-01-06 09:19:14 -0800

    made public

---
+++ dev/js/Modules/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+require('./../../../scss/style.scss');
+
+const App = () => (
+    <div>
+        <h2>User List</h2>
+        
+        <hr />
+        <h2>User Details</h2>
+         
+    </div>
+);
+
+export default App;
---
+++ dev/js/Modules/Header/Header.js
@@ -0,0 +1,3 @@
+// Write Header Bar Here, Export It and Import in dev/Modules/App/App.js
+// Write Css for this in dev/scss/Header.scss (Yes .scss file, No Differce Write as You write normal css)
+// Start Writing Component After This Line ,yeas i will
